0ce8f07d6c5363f463b25557ca8c594c34ee4236
Daniel <DanielP.SoftwareDev@gmail.com>
2024-10-15 18:37:11 -0400

    PropTypes for PopMessage and RenameDialog components
    Updated both components to adhere to the airbnb rules and added PropTypes where needed for type checking.

--- src/Systems/components/Dialogs/PopMessage.js
+++ src/Systems/components/Dialogs/PopMessage.js
@@ -1,21 +1,33 @@
+import useKeyDown from 'hooks/useKeyDown';
+import PropTypes from 'prop-types';
 
+const PopMessage = ({ children, setShowPopMessage }) => (
 
-const PopMessage = ({children, setShowPopMessage}) => {
-    return (
-       
-            <div className="min-w-screen min-h-screen z-50 absolute top-0 min-w-[100%] left-0" 
-            onClick={()=>setShowPopMessage(()=>false)}
-            >
-                <div 
-                className="transition-all duration-300 ease-in-out absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 border-brand-400/80 border-[0.05px] bg-gray-light rounded-lg  shadow-lg text-white z-50"
-                onClick={(e)=>e.stopPropagation()}
-                >
-                
-                {children}
-            </div>
-            </div>
-        
-    )
-}
+  <div
+    className="min-w-screen min-h-screen z-50 absolute top-0 min-w-[100%] left-0"
+    onClick={() => setShowPopMessage(() => false)}
+    onKeyDown={(e) => useKeyDown(e, 'Enter', [() => setShowPopMessage(() => false)])}
+    role="button"
+    tabIndex={0}
+  >
+    <div
+      className="transition-all duration-300 ease-in-out absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 border-brand-400/80 border-[0.05px] bg-gray-light rounded-lg  shadow-lg text-white z-50"
+      onClick={(e) => e.stopPropagation()}
+      onKeyDown={(e) => useKeyDown(e, 'Enter', [(y) => y.stopPropagation()])}
+      role="button"
+      tabIndex={0}
+
+    >
+
+      {children}
+    </div>
+  </div>
+
+);
+
+PopMessage.propTypes = {
+  children: PropTypes.node.isRequired,
+  setShowPopMessage: PropTypes.func.isRequired,
+};
 
 export default PopMessage;
--- src/Systems/components/Dialogs/RenameDialog.js
+++ src/Systems/components/Dialogs/RenameDialog.js
@@ -1,92 +1,89 @@
-import React, { useEffect, useState } from 'react';
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
 import { connectHelpGeneral } from 'hooks/InfoAndMeta/useSystems';
 import { useSystem } from 'contexts/Systems/SystemContext';
 import InputField from 'components/fields/InputField';
 
+const RenameDialog = ({
+  fileName, location, setShowPopMessage, connectGeneralArgs,
+}) => {
+  const { handleRenameContent } = useSystem();
+  const [formData, setFormData] = useState({
+    targetName: fileName,
+    sourceName: fileName,
+    sourceLocation: location,
+  });
 
-const RenameDialog = ({ fileName, location, setShowPopMessage, connectGeneralArgs }) => {
-    const initialData = {
-        targetName: fileName,
-        sourceName: fileName,
-        sourceLocation: location,
+  const [errors, setErrors] = useState('');
+
+  const handleConnect = async (e) => {
+    e.preventDefault();
+    const results = await handleRenameContent(formData);
+    if (results?.errors || results?.errors?.length) {
+      setErrors(() => results.errors.msg || results.errors[0].msg);
+      console.log('errors');
+    } else {
+      setShowPopMessage(false);
     }
-    const { createNewTerminal, handleRenameContent } = useSystem();
-    const [formData, setFormData] = useState({
-        targetName: fileName,
-        sourceName: fileName,
-        sourceLocation: location,
-    });
-    const [isLoading, setIsLoading] = useState(false);
-    const [showHelp, setShowHelp] = useState(false);
+  };
 
-    const [errors, setErrors] = useState('');
+  return (
+    <div className="max-w-[450px] w-[450px] ">
+      {/* help section */}
+      <div className="w-full pb-1">
+        {connectHelpGeneral(connectGeneralArgs)}
+      </div>
+      <div className="w-full flex ">
+        {/* <p className='text-white mr-2'>System name: </p> */}
+        <p className="text-gray-400">
+          Renaming "
+          {fileName}
+          "
+        </p>
+      </div>
 
-    const handleConnect = async (e) => {
-        e.preventDefault();
-        const results = await handleRenameContent(formData);
-        if (results?.errors || results?.errors?.length) {
-            setErrors(() => results.errors.msg || results.errors[0].msg)
-            console.log('errors')
-        } else {
-            setShowPopMessage(false);
-        }
-    }
-
-    return (
-        <div className='max-w-[450px] w-[450px] '>
-            {/* help section */}
-            <div className='w-full pb-1'>
-                {connectHelpGeneral(connectGeneralArgs)}
-            </div>
-            <div className='w-full flex '>
-                {/* <p className='text-white mr-2'>System name: </p> */}
-                <p className='text-gray-400'>Renaming "{fileName}"</p>
-            </div>
-
-            <InputField
-                variant="auth"
-                extra=" mb-4 min-w-[400px]"
-                height={'50px'}
-                label=""
+      <InputField
+        variant="auth"
+        extra=" mb-4 min-w-[400px]"
+        height="50px"
+        label=""
                 // placeholder="[ New Name ]"
-                id="targetName"
-                type="text"
-                value={formData.targetName}
-                hasLabel={false}
+        id="targetName"
+        type="text"
+        value={formData.targetName}
+        hasLabel={false}
                 // autocomplete="off"
                 // hasBorder={false}
-                onChange={(e) => setFormData({ ...formData, targetName: e.target.value })}
-            />
+        onChange={(e) => setFormData({ ...formData, targetName: e.target.value })}
+      />
 
-            <button
-                className={
+      <button
+        className={
                     `
-                    linear w-[110px] rounded-xl ${!formData.targetName || isLoading ? 'bg-gray-400' : 'bg-brand-400 hover:bg-brand-600 active:bg-brand-700'} py-[10px] text-sm text-white transition duration-200 
+                    linear w-[110px] rounded-xl ${!formData.targetName ? 'bg-gray-400' : 'bg-brand-400 hover:bg-brand-600 active:bg-brand-700'} py-[10px] text-sm text-white transition duration-200 
                     `
                 }
-                disabled={!formData.targetName || isLoading}
-                onClick={handleConnect}
-            >
-                Rename
-            </button>
+        disabled={!formData.targetName}
+        onClick={handleConnect}
+        type="button"
+      >
+        Rename
+      </button>
 
-            <button
-                className='ml-6 linear w-[110px] rounded-xl bg-red-700 py-[10px] text-sm text-white transition duration-200 hover:bg-red-900 active:bg-brand-700'
-                disabled={isLoading}
-                onClick={() => setShowPopMessage(false)}
-            >
-                Cancel
-            </button>
+      <button
+        className="ml-6 linear w-[110px] rounded-xl bg-red-700 py-[10px] text-sm text-white transition duration-200 hover:bg-red-900 active:bg-brand-700"
+        onClick={() => setShowPopMessage(false)}
+        type="button"
+      >
+        Cancel
+      </button>
 
-            
-            <p className="mt-2 mb-2 text-base text-red-600">
-                {errors ? `${errors} ☝` : ''}
-            </p>
+      <p className="mt-2 mb-2 text-base text-red-600">
+        {errors ? `${errors} ☝` : ''}
+      </p>
 
-
-
-        </div>
-    )
-}
+    </div>
+  );
+};
 
 export default RenameDialog;
